5dec416e208ff823a67860eb23216f37ce2c9710
押切郁也 <fumi.topone@gmail.com>
2021-01-24 22:19:16 +0900

    first Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {HashRouter as Router,Route,Link}from 'react-router-dom';
+import Home from './components/Home'
+import About from './components/About'
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+     <h1>Hello World</h1>
+     <Router>
+       <ul>
+         <li><Link to="/">home</Link></li>
+         <li><Link to="/about">about</Link></li>
+       </ul>
+       <Route path="/" exact component={Home}></Route>
+       <Route path="/about/:id" exact component={About}></Route>
+     </Router>
+     
+   </div>
   );
 }
 
---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About(props) {
+   
+    const match = props.match;
+    console.log(match)
+  return (
+   
+     <h1 >Aboutです</h1>
+   
+  );
+}
+
+export default About;
